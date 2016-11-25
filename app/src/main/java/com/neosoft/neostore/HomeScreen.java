e10b3ca44536b1aebfb5b004795f2e9fcce52b73
ninad.thakare <ninad.thakare@neosofttech.com>
2016-11-25 21:25:15 +0530

    drawer layout

---
+++ app/src/main/java/com/neosoft/neostore/HomeScreen.java
@@ -0,0 +1,19 @@
+package com.neosoft.neostore;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by webwerks1 on 25/11/16.
+ */
+
+public class HomeScreen extends AppCompatActivity{
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_drawer);
+
+
+    }
+
+}
