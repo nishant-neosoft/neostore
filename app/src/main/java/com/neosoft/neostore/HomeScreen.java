b6e96a100ec52b605b9a49f8f22c309289e236c0
ninad.thakare <ninad.thakare@neosofttech.com>
2016-11-29 16:07:27 +0530

    counter addition

--- app/src/main/java/com/neosoft/neostore/HomeScreen.java
+++ app/src/main/java/com/neosoft/neostore/HomeScreen.java
@@ -1,19 +1,50 @@
 package com.neosoft.neostore;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.view.MenuItemCompat;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Gravity;
+import android.view.MenuItem;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
-/**
- * Created by webwerks1 on 25/11/16.
- */
-
-public class HomeScreen extends AppCompatActivity{
+public class HomeScreen extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
+    TextView mycart;
+    LinearLayout badge_layout;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_drawer);
-
-
+        NavigationView navigationView = (NavigationView) findViewById(R.id.navlayout);
+        navigationView.setNavigationItemSelectedListener(this);
+        badge_layout = (LinearLayout) MenuItemCompat.getActionView(navigationView.getMenu().findItem(R.id.nav_mycart));
+        badge_layout.setGravity(Gravity.CENTER);
+        mycart = new TextView(this);
+        mycart.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT));
+        mycart.setText("3");
+        mycart.setBackgroundResource(R.drawable.roundtextview);
+        mycart.setGravity(Gravity.CENTER);
+        mycart.setTextColor(getResources().getColor(R.color.colorWhite));
+        badge_layout.addView(mycart);
     }
-
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
 }
